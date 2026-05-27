dd62618df0762d44229530c8ba9c908d728838f7
riccdigital-code <riccdigital@gmail.com>
2026-05-27 13:48:12 +0300

    latest polish

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,16 +1,13 @@
-import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-export const getRouter = () => {
-  const queryClient = new QueryClient();
+export const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+});
 
-  const router = createRouter({
-    routeTree,
-    context: { queryClient },
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
-  });
-
-  return router;
-};
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
